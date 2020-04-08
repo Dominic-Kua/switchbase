931c49b06c0b709da94fec9a9688f4b16a2d07fa
Dominic Kua <dominic@kua.org.uk>
2020-04-08 15:10:48 +0100

    Adding some basic structure and files

--- app.py
+++ app.py
@@ -8,5 +8,8 @@ DEBUG = False or os.environ["FLASK_DEBUG"]
 app = flask.Flask(__name__)
 
 def main():
-
     app.run(debug=DEBUG)
+
+@app.route("/")
+def hello_world():
+    return "<h1>The Switch Database</h1>"
